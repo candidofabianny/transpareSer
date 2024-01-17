70bcdb75f274ac57139976a311abd8500cc70328
candidofabianny <candidofabianny@gmail.com>
2024-01-17 12:32:56 -0300

    atualizar tamanhos sm

--- transpareser/src/components/home/Galeria.tsx
+++ transpareser/src/components/home/Galeria.tsx
@@ -9,11 +9,11 @@ import Image from "next/image";
 const Galeria = () => {
   return (
     <section id="home"
-    className="bg-[url('../assets/images/home/bggaleria.png')] bg-cover bg-center flex flex-col items-center justify-center min-h-[600px] w-full">    
-        <div className="bg-black/60 min-h-[600px] w-full flex flex-col md:flex-row justify-center items-end">
+    className="bg-[url('../assets/images/home/bggaleria.png')] bg-cover bg-center flex flex-col items-center justify-center min-h-[500px] w-full">    
+        <div className="bg-black/60 min-h-[500px] w-full flex flex-col md:flex-row justify-center items-end">
         <Image
           alt="gallery"
-          className="absolute block h-[550px] w-[550px] rounded-lg object-cover"
+          className="absolute block h-[500px] md:h-[550px] w-[500px] md:w-[550px] rounded-lg object-cover"
           src={totalframe} />
        
     </div>
