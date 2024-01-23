7def085349ae79eb460e078ebe1025fc44d43ada
candidofabianny <candidofabianny@gmail.com>
2024-01-22 21:21:41 -0300

    ajustes

--- transpareser/src/components/sobre/BannerEmpresa.tsx
+++ transpareser/src/components/sobre/BannerEmpresa.tsx
@@ -4,7 +4,7 @@ import { voigante } from "@/assets/fonts/fonts";
 const BannerEmpresa = () => {
   return (
     <section id="banner"
-    className="bg-[url('../assets/images/sobre/bginsta.png')] bg-center bg-cover bg-fixed flex flex-col items-center justify-center min-h-[177px] md:min-h-[226px] w-full">    
+    className="bg-[url('../assets/images/sobre/bginsta.png')] bgfixed bg-center bg-cover bg-fixed flex flex-col items-center justify-center min-h-[177px] md:min-h-[226px] w-full">    
         <div className="flex flex-col items-center justify-center py-4">
         <div className={`${voigante.className}`}>
         <h1 className="text-white text-[1.8rem] md:text-[2.4rem] lg:text-[2.8rem] px-2 pb-2 md:pb-4 lg:pb-6 text-center leading-8">
